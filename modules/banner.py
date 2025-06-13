e7ae851787953211b0431d82239a6cd75ae72bbc
swatiai11414 <168453519+swatiai11414@users.noreply.github.com>
2025-06-13 12:43:40 +0530

    Update banner.py

--- modules/banner.py
+++ modules/banner.py
@@ -1,21 +1,22 @@
-from colorama import Fore,Back,Style
-import platform,os
+from colorama import Fore, Back, Style
+import platform, os
 
 OsName = platform.uname()[0]
 
 def banner():
     if OsName == "Windows":
-      os.system("cls")
+        os.system("cls")
     else:
-      os.system("clear")
-    print(Fore.LIGHTWHITE_EX+" (               )   (       *                (                     )       (")   
-    print(Fore.LIGHTWHITE_EX+" )\ )  *   )  ( /(   )\ )  (  `           (   )\ )       (       ( /(       )\ )")  
-    print(Fore.LIGHTWHITE_EX+"(()/(` )  /(  )\()) (()/(  )\))(        ( )\ (()/( (     )\      )\()) (   (()/( " )
-    print(Fore.LIGHTWHITE_EX+"/(_))( )(_))((_)\   /(_)_)()\       ___  )((_) /(_)))\ ((((_)(  |((_)\  )\   /(_)) ")
-    print(Fore.CYAN+" (_)) (_(_())   ((_) (_))  (_()((_)|___|((_)_ (_)) ((_) )\ _ )\ |_ ((_)((_) (_)) "  )
-    print(Fore.CYAN+"/ __||_   _|  / _ \ | _ \ |  \/  |      | _ )| _ \| __|(_)_\(_)| |/ / | __|| _ \ " )
-    print(Fore.CYAN+"\__ \  | |   | (_) ||   / | |\/| |      | _ \|   /| _|  / _ \    ' <  | _| |   / " )
-    print(Fore.CYAN+"|___/  |_|    \___/ |_|_\ |_|  |_|      |___/|_|_\|___|/_/ \_\  _|\_\ |___||_|_\\")
+        os.system("clear")
+
+    print(Fore.LIGHTWHITE_EX + r" (               )   (       *                (                     )       (")   
+    print(Fore.LIGHTWHITE_EX + r" )\ )  *   )  ( /(   )\ )  (  `           (   )\ )       (       ( /(       )\ )")  
+    print(Fore.LIGHTWHITE_EX + r"(()/(` )  /(  )\()) (()/(  )\))(        ( )\ (()/( (     )\      )\()) (   (()/( ") 
+    print(Fore.LIGHTWHITE_EX + r"/(_))( )(_))((_)\   /(_)_)()\       ___  )((_) /(_)))\ ((((_)(  |((_)\  )\   /(_)) ")
+    print(Fore.CYAN + r" (_)) (_(_())   ((_) (_))  (_()((_)|___|((_)_ (_)) ((_) )\ _ )\ |_ ((_)((_) (_)) ") 
+    print(Fore.CYAN + r"/ __||_   _|  / _ \ | _ \ |  \/  |      | _ )| _ \| __|(_)_\(_)| |/ / | __|| _ \ ") 
+    print(Fore.CYAN + r"\__ \  | |   | (_) ||   / | |\/| |      | _ \|   /| _|  / _ \    ' <  | _| |   / ") 
+    print(Fore.CYAN + r"|___/  |_|    \___/ |_|_\ |_|  |_|      |___/|_|_\|___|/_/ \_\  _|\_\ |___||_|_\\")
     print(Style.RESET_ALL)
 
 banner()
